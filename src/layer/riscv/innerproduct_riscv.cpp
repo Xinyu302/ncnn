800e1de948c8a6d6e9c2773c0d7b81f23cc9012f
Xinyu Yang <ltyxy@buaa.edu.cn>
2024-02-14 15:59:57 +0800

    modify header (#8)

--- src/layer/riscv/innerproduct_riscv.cpp
+++ src/layer/riscv/innerproduct_riscv.cpp
@@ -1,5 +1,6 @@
 // Tencent is pleased to support the open source community by making ncnn available.
 //
+// Copyright (C) 2024 Xinyu302. All rights reserved.
 // Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
 //
 // Licensed under the BSD 3-Clause License (the "License"); you may not use this file except

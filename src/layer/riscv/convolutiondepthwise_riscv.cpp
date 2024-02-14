ca8b112e79cd41fe7bcb817a1a7fe1d18705b584
yxy <summerhh54@gmail.com>
2024-02-14 13:27:58 +0000

    delete comment

--- src/layer/riscv/convolutiondepthwise_riscv.cpp
+++ src/layer/riscv/convolutiondepthwise_riscv.cpp
@@ -244,27 +244,6 @@ int ConvolutionDepthWise_riscv::forward(const Mat& bottom_blob, Mat& top_blob, c
     {
         fprintf(stderr, "ConvolutionDepthWise_riscv::forward int8 scale is called\n");
         return forward_int8(bottom_blob, top_blob, opt);
-        // Mat bottom_blob_unpacked = bottom_blob;
-        // if (bottom_blob.elempack != 1)
-        // {
-        //     Option opt_pack1 = opt;
-        //     opt_pack1.blob_allocator = opt.workspace_allocator;
-
-        //     convert_packing(bottom_blob, bottom_blob_unpacked, 1, opt_pack1);
-        // }
-
-        // Mat bottom_blob_unpacked_fp32 = bottom_blob_unpacked;
-        // if (bottom_blob_unpacked.elembits() == 16)
-        // {
-        //     Option opt_pack1 = opt;
-        //     opt_pack1.blob_allocator = opt.workspace_allocator;
-
-        //     cast_float16_to_float32(bottom_blob_unpacked, bottom_blob_unpacked_fp32, opt_pack1);
-        // }
-
-        // Option opt_unpacked = opt;
-        // opt_unpacked.use_packing_layout = false;
-        // return ConvolutionDepthWise::forward_int8(bottom_blob_unpacked_fp32, top_blob, opt_unpacked);
     }
 #endif
 

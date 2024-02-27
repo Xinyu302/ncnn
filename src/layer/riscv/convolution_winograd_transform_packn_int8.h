8e14afc5a375aeaa4d92daa19f922c6813960bdd
yxy <summerhh54@gmail.com>
2024-02-27 11:47:25 +0000

    finish winograd23 int8 transform

--- src/layer/riscv/convolution_winograd_transform_packn_int8.h
+++ src/layer/riscv/convolution_winograd_transform_packn_int8.h
@@ -291,56 +291,56 @@ static void conv3x3s1_winograd23_transform_input_packn_int8_rvv(const Mat& botto
         Mat img0_tm = bottom_blob_tm.channel(q);
 
         // NOTE c99 variable length array
-        int8_t tmp[4][4][packn];
+        int16_t tmp[4][4][packn];
 
         // tile
         for (int i = 0; i < h_tiles; i++)
         {
             for (int j = 0; j < w_tiles; j++)
             {
-                const int8_t* r0 = img0.row<const int8_t>(i * 2) + (j * 2) * packn;
+                const int16_t* r0 = img0.row<const int16_t>(i * 2) + (j * 2) * packn;
 
                 for (int m = 0; m < 4; m++)
                 {
-                    vfloat16m1_t _r00 = vle16_v_f16m1(r0, vl);
-                    vfloat16m1_t _r01 = vle16_v_f16m1(r0 + packn, vl);
-                    vfloat16m1_t _r02 = vle16_v_f16m1(r0 + packn * 2, vl);
-                    vfloat16m1_t _r03 = vle16_v_f16m1(r0 + packn * 3, vl);
+                    vint16m1_t _r00 = vle16_v_i16m1(r0, vl);
+                    vint16m1_t _r01 = vle16_v_i16m1(r0 + packn, vl);
+                    vint16m1_t _r02 = vle16_v_i16m1(r0 + packn * 2, vl);
+                    vint16m1_t _r03 = vle16_v_i16m1(r0 + packn * 3, vl);
 
-                    vfloat16m1_t _tmp0m = vfsub_vv_f16m1(_r00, _r02, vl);
-                    vfloat16m1_t _tmp1m = vfadd_vv_f16m1(_r01, _r02, vl);
-                    vfloat16m1_t _tmp2m = vfsub_vv_f16m1(_r02, _r01, vl);
-                    vfloat16m1_t _tmp3m = vfsub_vv_f16m1(_r03, _r01, vl);
+                    vint16m1_t _tmp0m = vsub_vv_i16m1(_r00, _r02, vl);
+                    vint16m1_t _tmp1m = vadd_vv_i16m1(_r01, _r02, vl);
+                    vint16m1_t _tmp2m = vsub_vv_i16m1(_r02, _r01, vl);
+                    vint16m1_t _tmp3m = vsub_vv_i16m1(_r03, _r01, vl);
 
-                    vse16_v_f16m1(tmp[0][m], _tmp0m, vl);
-                    vse16_v_f16m1(tmp[1][m], _tmp1m, vl);
-                    vse16_v_f16m1(tmp[2][m], _tmp2m, vl);
-                    vse16_v_f16m1(tmp[3][m], _tmp3m, vl);
+                    vse16_v_i16m1(tmp[0][m], _tmp0m, vl);
+                    vse16_v_i16m1(tmp[1][m], _tmp1m, vl);
+                    vse16_v_i16m1(tmp[2][m], _tmp2m, vl);
+                    vse16_v_i16m1(tmp[3][m], _tmp3m, vl);
 
                     r0 += w * packn;
                 }
 
-                int8_t* r0_tm_0 = (int8_t*)img0_tm + (i * w_tiles + j) * packn;
-                int8_t* r0_tm_1 = r0_tm_0 + tiles * packn;
-                int8_t* r0_tm_2 = r0_tm_0 + tiles * packn * 2;
-                int8_t* r0_tm_3 = r0_tm_0 + tiles * packn * 3;
+                int16_t* r0_tm_0 = (int16_t*)img0_tm + (i * w_tiles + j) * packn;
+                int16_t* r0_tm_1 = r0_tm_0 + tiles * packn;
+                int16_t* r0_tm_2 = r0_tm_0 + tiles * packn * 2;
+                int16_t* r0_tm_3 = r0_tm_0 + tiles * packn * 3;
 
                 for (int m = 0; m < 4; m++)
                 {
-                    vfloat16m1_t _tmp00 = vle16_v_f16m1(tmp[m][0], vl);
-                    vfloat16m1_t _tmp01 = vle16_v_f16m1(tmp[m][1], vl);
-                    vfloat16m1_t _tmp02 = vle16_v_f16m1(tmp[m][2], vl);
-                    vfloat16m1_t _tmp03 = vle16_v_f16m1(tmp[m][3], vl);
+                    vint16m1_t _tmp00 = vle16_v_i16m1(tmp[m][0], vl);
+                    vint16m1_t _tmp01 = vle16_v_i16m1(tmp[m][1], vl);
+                    vint16m1_t _tmp02 = vle16_v_i16m1(tmp[m][2], vl);
+                    vint16m1_t _tmp03 = vle16_v_i16m1(tmp[m][3], vl);
 
-                    vfloat16m1_t _r0tm0 = vfsub_vv_f16m1(_tmp00, _tmp02, vl);
-                    vfloat16m1_t _r0tm1 = vfadd_vv_f16m1(_tmp01, _tmp02, vl);
-                    vfloat16m1_t _r0tm2 = vfsub_vv_f16m1(_tmp02, _tmp01, vl);
-                    vfloat16m1_t _r0tm3 = vfsub_vv_f16m1(_tmp03, _tmp01, vl);
+                    vint16m1_t _r0tm0 = vsub_vv_i16m1(_tmp00, _tmp02, vl);
+                    vint16m1_t _r0tm1 = vadd_vv_i16m1(_tmp01, _tmp02, vl);
+                    vint16m1_t _r0tm2 = vsub_vv_i16m1(_tmp02, _tmp01, vl);
+                    vint16m1_t _r0tm3 = vsub_vv_i16m1(_tmp03, _tmp01, vl);
 
-                    vse16_v_f16m1(r0_tm_0, _r0tm0, vl);
-                    vse16_v_f16m1(r0_tm_1, _r0tm1, vl);
-                    vse16_v_f16m1(r0_tm_2, _r0tm2, vl);
-                    vse16_v_f16m1(r0_tm_3, _r0tm3, vl);
+                    vse16_v_i16m1(r0_tm_0, _r0tm0, vl);
+                    vse16_v_i16m1(r0_tm_1, _r0tm1, vl);
+                    vse16_v_i16m1(r0_tm_2, _r0tm2, vl);
+                    vse16_v_i16m1(r0_tm_3, _r0tm3, vl);
 
                     r0_tm_0 += tiles * packn * 4;
                     r0_tm_1 += tiles * packn * 4;
@@ -352,7 +352,7 @@ static void conv3x3s1_winograd23_transform_input_packn_int8_rvv(const Mat& botto
     }
 }
 
-static void conv3x3s1_winograd23_transform_output_packn_int8_rvv(const Mat& top_blob_tm, Mat& top_blob, const Mat& bias, const Option& opt)
+static void conv3x3s1_winograd23_transform_output_packn_int8_rvv(const Mat& top_blob_tm, Mat& top_blob, const Option& opt)
 {
     const int packn = csrr_vlenb() / 2;
     const size_t vl = vsetvl_e16m1(packn);
@@ -365,7 +365,7 @@ static void conv3x3s1_winograd23_transform_output_packn_int8_rvv(const Mat& top_
     const int h_tiles = outh / 2;
     const int tiles = w_tiles * h_tiles;
 
-    const int8_t* biasptr = bias;
+    // const int8_t* biasptr = bias;
 
     // const float otm[2][4] = {
     //     {1.0f,  1.0f,  1.0f,  0.0f},
@@ -381,35 +381,35 @@ static void conv3x3s1_winograd23_transform_output_packn_int8_rvv(const Mat& top_
         const Mat out0_tm = top_blob_tm.channel(p);
         Mat out0 = top_blob.channel(p);
 
-        vfloat16m1_t _bias0 = biasptr ? vle16_v_f16m1(biasptr + p * packn, vl) : vfmv_v_f_f16m1(0.f, vl);
+        // vint16m1_t _bias0 = biasptr ? vle16_v_i16m1(biasptr + p * packn, vl) : vfmv_v_f_f16m1(0.f, vl);
 
         // NOTE variable length array
-        int8_t tmp[2][4][packn];
+        int16_t tmp[2][4][packn];
 
         // tile
         for (int i = 0; i < h_tiles; i++)
         {
             for (int j = 0; j < w_tiles; j++)
             {
-                const int8_t* output0_tm_0 = (const int8_t*)out0_tm + (i * w_tiles + j) * packn;
-                const int8_t* output0_tm_1 = output0_tm_0 + tiles * packn;
-                const int8_t* output0_tm_2 = output0_tm_0 + tiles * packn * 2;
-                const int8_t* output0_tm_3 = output0_tm_0 + tiles * packn * 3;
+                const int16_t* output0_tm_0 = (const int16_t*)out0_tm + (i * w_tiles + j) * packn;
+                const int16_t* output0_tm_1 = output0_tm_0 + tiles * packn;
+                const int16_t* output0_tm_2 = output0_tm_0 + tiles * packn * 2;
+                const int16_t* output0_tm_3 = output0_tm_0 + tiles * packn * 3;
 
-                int8_t* output0 = out0.row<int8_t>(i * 2) + (j * 2) * packn;
+                int16_t* output0 = out0.row<int16_t>(i * 2) + (j * 2) * packn;
 
                 for (int m = 0; m < 4; m++)
                 {
-                    vfloat16m1_t _out0tm0 = vle16_v_f16m1(output0_tm_0, vl);
-                    vfloat16m1_t _out0tm1 = vle16_v_f16m1(output0_tm_1, vl);
-                    vfloat16m1_t _out0tm2 = vle16_v_f16m1(output0_tm_2, vl);
-                    vfloat16m1_t _out0tm3 = vle16_v_f16m1(output0_tm_3, vl);
+                    vint16m1_t _out0tm0 = vle16_v_i16m1(output0_tm_0, vl);
+                    vint16m1_t _out0tm1 = vle16_v_i16m1(output0_tm_1, vl);
+                    vint16m1_t _out0tm2 = vle16_v_i16m1(output0_tm_2, vl);
+                    vint16m1_t _out0tm3 = vle16_v_i16m1(output0_tm_3, vl);
 
-                    vfloat16m1_t _tmp0m = vfadd_vv_f16m1(vfadd_vv_f16m1(_out0tm0, _out0tm1, vl), _out0tm2, vl);
-                    vfloat16m1_t _tmp1m = vfadd_vv_f16m1(vfsub_vv_f16m1(_out0tm1, _out0tm2, vl), _out0tm3, vl);
+                    vint16m1_t _tmp0m = vadd_vv_i16m1(vadd_vv_i16m1(_out0tm0, _out0tm1, vl), _out0tm2, vl);
+                    vint16m1_t _tmp1m = vadd_vv_i16m1(vsub_vv_i16m1(_out0tm1, _out0tm2, vl), _out0tm3, vl);
 
-                    vse16_v_f16m1(tmp[0][m], _tmp0m, vl);
-                    vse16_v_f16m1(tmp[1][m], _tmp1m, vl);
+                    vse16_v_i16m1(tmp[0][m], _tmp0m, vl);
+                    vse16_v_i16m1(tmp[1][m], _tmp1m, vl);
 
                     output0_tm_0 += tiles * packn * 4;
                     output0_tm_1 += tiles * packn * 4;
@@ -419,16 +419,16 @@ static void conv3x3s1_winograd23_transform_output_packn_int8_rvv(const Mat& top_
 
                 for (int m = 0; m < 2; m++)
                 {
-                    vfloat16m1_t _tmp00 = vle16_v_f16m1(tmp[m][0], vl);
-                    vfloat16m1_t _tmp01 = vle16_v_f16m1(tmp[m][1], vl);
-                    vfloat16m1_t _tmp02 = vle16_v_f16m1(tmp[m][2], vl);
-                    vfloat16m1_t _tmp03 = vle16_v_f16m1(tmp[m][3], vl);
+                    vint16m1_t _tmp00 = vle16_v_i16m1(tmp[m][0], vl);
+                    vint16m1_t _tmp01 = vle16_v_i16m1(tmp[m][1], vl);
+                    vint16m1_t _tmp02 = vle16_v_i16m1(tmp[m][2], vl);
+                    vint16m1_t _tmp03 = vle16_v_i16m1(tmp[m][3], vl);
 
-                    vfloat16m1_t _out00 = vfadd_vv_f16m1(_bias0, vfadd_vv_f16m1(vfadd_vv_f16m1(_tmp00, _tmp01, vl), _tmp02, vl), vl);
-                    vfloat16m1_t _out01 = vfadd_vv_f16m1(_bias0, vfadd_vv_f16m1(vfsub_vv_f16m1(_tmp01, _tmp02, vl), _tmp03, vl), vl);
+                    vint16m1_t _out00 = vadd_vv_i16m1(vadd_vv_i16m1(_tmp00, _tmp01, vl), _tmp02, vl);
+                    vint16m1_t _out01 = vadd_vv_i16m1(vsub_vv_i16m1(_tmp01, _tmp02, vl), _tmp03, vl);
 
-                    vse16_v_f16m1(output0, _out00, vl);
-                    vse16_v_f16m1(output0 + packn, _out01, vl);
+                    vse16_v_i16m1(output0, _out00, vl);
+                    vse16_v_i16m1(output0 + packn, _out01, vl);
 
                     output0 += outw * packn;
                 }
